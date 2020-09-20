8db4cecc7c2cfcab180e8d8c9a485116ce3dc9d0
mattyocode <matthewoliver@live.co.uk>
2020-09-20 15:54:17 +0100

    First commit - deleted non-python folders

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 import unittest
+import pytest
 
 from gilded_rose import Item, GildedRose
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+def test_foo():
+    items = [Item("foo", 0, 0)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert "foo" == items[0].name
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
